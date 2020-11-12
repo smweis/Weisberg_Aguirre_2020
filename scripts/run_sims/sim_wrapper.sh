d72f1ea57019abad96a1e2cf2a978ee5ff4a2739
SteveW Hipergator <smweis@gmail.com>
2020-11-12 11:54:54 -0500

    cleaning up deploy_sim and sim_wrapper for new batches of sims

--- scripts/run_sims/sim_wrapper.sh
+++ scripts/run_sims/sim_wrapper.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
-
+# A wrapper for deploy_sim.sh to run that script in a loop for parameters you want to vary.
 for RUN in {0001..0050}
 do
   for nOutcomes in 7 15
   do
     for noise in .05 .15 .25
     do
-      sbatch deploy_sim.sh $nOutcomes $noise false random_${nOutcomes}_${noise}_${RUN}
-      sleep .1
-      sbatch deploy_sim.sh $nOutcomes $noise true qpControl_${nOutcomes}_${noise}_${RUN}
-      sleep .1
+      for trialLength in 8 12
+      do
+        for TR in 800 1600
+        do
+          sbatch deploy_sim.sh $nOutcomes $noise $trialLength $TR false random_${nOutcomes}_nOutcomes_${noise}_noise_${RUN}
+          sleep .1
+          sbatch deploy_sim.sh $nOutcomes $noise $trialLength $TR true qpControl_${nOutcomes}_nOutcomes_${noise}_noise_${RUN}
+          sleep .1
+        done
+      done
     done
   done
 done
